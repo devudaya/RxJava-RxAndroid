9c5f6c8a5c3eaae3198f82a1a54ecfbbfe79571d
Udaya Kahandawala <niroshan127@gmail.com>
2020-04-19 14:39:51 +0530

    first RxJava and RxAndroid

---
+++ app/src/main/java/com/example/rxjava_rxandroid/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.rxjava_rxandroid
+
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val taskObservable: Observable<Task> = Observable.fromIterable(DataSource.getTaskList())
+            .subscribeOn(Schedulers.io())
+            .filter{task ->
+                Log.d(TAG , "onNext: : " + Thread.currentThread().name)
+                Thread.sleep(1000)
+                task.isComplete
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+
+        taskObservable.subscribe(object : Observer<Task>{
+            override fun onComplete() {
+                Log.d(TAG , "onComplete: task completed")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG , "onSubscribe: : called")
+            }
+
+            override fun onNext(t: Task) {
+                Log.d(TAG , "onNext: : " + t.description)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG , "onError: : ", e)
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/rxjava_rxandroid/Task.kt
@@ -0,0 +1,7 @@
+package com.example.rxjava_rxandroid
+
+data class Task(
+    var description: String,
+    var isComplete: Boolean,
+    var priority: Int
+)
